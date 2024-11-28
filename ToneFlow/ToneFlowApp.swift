5cce8d005f01b298e387f636aa9f6774612f9ad0
Wesley Grubbs <wes@pitchinteractive.com>
2024-11-27 17:26:50 -0800

    Initial Commit

---
+++ ToneFlow/ToneFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToneFlowApp.swift
+//  ToneFlow
+//
+//  Created by Wesley Grubbs on 11/27/24.
+//
+
+import SwiftUI
+
+@main
+struct ToneFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
